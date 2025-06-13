0be08226fe518bf7dd21a7ac1c86220ae6694aac
Luis Filipe Fialho <luisfilipefialho@proton.me>
2025-06-13 12:41:28 -0300

    feat(interface): add Bike, Customer and Rental

---
+++ src/BikeRental.Application/Interfaces/IBikeService.cs
@@ -0,0 +1,13 @@
+﻿using BikeRental.Application.DTOs.Bike;
+using BikeRental.Domain.Entities;
+
+namespace BikeRental.Application.Interfaces;
+
+public interface IBikeService
+{
+    Task CreateAsync(CreateBikeRequest request);
+    Task<GetBikeResponse> GetByIdAsync(Guid id);
+    Task<IEnumerable<GetBikeResponse>> GetAllAsync(string? plateFilter);
+    Task UpdatePlateAsync(Guid id, UpdateBikeRequest request);
+    Task DeleteAsync(Guid id);
+}
---
+++ src/BikeRental.Application/Interfaces/IRentalService.cs
@@ -0,0 +1,10 @@
+﻿using BikeRental.Application.DTOs.Rental;
+
+namespace BikeRental.Application.Interfaces;
+
+public interface IRentalService
+{
+    Task CreateAsync(CreateRentalRequest request);
+    Task<GetRentalResponse?> GetByIdAsync(Guid id);
+    Task UpdateReturnDateAsync(Guid id, UpdateRentalRequest request);
+}
